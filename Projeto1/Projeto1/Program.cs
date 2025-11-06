a29c2b16a1b125b0d54dfffc64883db7a1d3b851
brenogaudencio <brenobichels123@gmail.com>
2025-11-06 19:19:06 -0300

    saída de dados

--- Projeto1/Projeto1/Program.cs
+++ Projeto1/Projeto1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 
+
 namespace CSharp
 {
     internal class Program
@@ -8,12 +9,29 @@ namespace CSharp
         static void Main(string[] args)
         {
             double x = 10.35784;
+            char z = 'F';
+            int y = 39;
+            string w = "Maria";
 
+
+
+            Console.Write("Olá Mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Até mais.");
+
+            Console.WriteLine();
             Console.WriteLine(x);
             Console.WriteLine(x.ToString("F2"));
             Console.WriteLine(x.ToString("F4"));
-
             Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("Meu nome é " + w + ", tenho " + y + " anos e sou do sexo " + z + ".");
+
+            Console.WriteLine();
+            Console.WriteLine("RESULTADO =  R$" + x);
+            Console.WriteLine("O valor do troco é = R$" + x);
+            Console.WriteLine("O valor do troco é = R$" + x.ToString("F2"));
         }
     }
 }
--- Projeto1/Projeto1/obj/Release/net8.0/Projeto1.AssemblyInfo.cs
+++ Projeto1/Projeto1/obj/Release/net8.0/Projeto1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d180dbd97e9bf8145bf125c1ca4e10ba9f3f2677")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto1")]
