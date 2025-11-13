f54f2b8b045f389abb86cc67b09cb2d2d78f6348
brenogaudencio <brenobichels123@gmail.com>
2025-11-13 20:48:59 -0300

    split

---
+++ EntradaDeDadosPT1/EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EntradaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+
+
+
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);   
+        }
+    }
+}
--- EntradaDeDadosPT1/EntradaDeDados/EntradaDeDados/obj/Debug/net8.0/EntradaDeDados.AssemblyInfo.cs
+++ EntradaDeDadosPT1/EntradaDeDados/EntradaDeDados/obj/Debug/net8.0/EntradaDeDados.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProcessamentoDeDados")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c550fc565c0eed5812f120f98b90ac445ea11e8c")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProcessamentoDeDados")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProcessamentoDeDados")]
+[assembly: System.Reflection.AssemblyProductAttribute("EntradaDeDados")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
