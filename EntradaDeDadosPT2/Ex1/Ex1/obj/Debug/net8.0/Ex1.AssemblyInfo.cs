5a4a81c8228f32c1ac8ec7b38f16db81965939f8
brenogaudencio <brenobichels123@gmail.com>
2025-11-21 18:50:50 -0300

    Exercício resolvido 2

--- EntradaDeDadosPT2/Ex1/Ex1/obj/Debug/net8.0/Ex1.AssemblyInfo.cs
+++ EntradaDeDadosPT2/Ex1/Ex1/obj/Debug/net8.0/Ex1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81b1593e60348486c126fb8321e1641f039ea8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1829d3e3d840493b2a841beff0a9b43f1f45dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ExercicioResolvido2/ExercicioResolvido2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
+using System.Globalization;
+
+namespace ExResolvido2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, altura, area, perimetro, diagonal, x;
+
+            altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = b * altura;
+            perimetro = 2 * (b + altura);
+            x = (b * b) + (altura * altura);
+
+            diagonal = Math.Sqrt(x);
+
+            Console.WriteLine("Area = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
