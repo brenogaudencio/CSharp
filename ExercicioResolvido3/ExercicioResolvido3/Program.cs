c34d770060fc13bf6ae22947cbb63e42858cc1c6
brenogaudencio <brenobichels123@gmail.com>
2025-11-22 08:05:02 -0300

    Exercicio Resolvido 3

---
+++ ExercicioResolvido3/ExercicioResolvido3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomeM = Console.ReadLine().Split(' ');
+            string nome = nomeM[0];
+            int idade = int.Parse(nomeM[1]);
+
+            string[] NomeJ = Console.ReadLine().Split(' ');
+            string nome2 = NomeJ[0];
+            int idade2 = int.Parse(NomeJ[1]);
+
+            double idadeMedia = (idade + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de " + nome + " e " + nome2 + " é de " + idadeMedia.ToString(CultureInfo.InvariantCulture) + " anos.");
+        }
+    }
+}   
