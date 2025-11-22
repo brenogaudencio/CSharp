142d5101a940839a959882236ddeeca6c529236f
brenogaudencio <brenobichels123@gmail.com>
2025-11-22 08:36:18 -0300

    exercicio resolvido 4

---
+++ ExercicioResolvido4/ExercicioResolvido4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int segundosHora = 3600;
+
+            int horas = numero / segundosHora;
+            int minutos = (numero % segundosHora) / 60;
+            int sec = (numero % segundosHora) % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + sec);
+        }
+    }
+}
