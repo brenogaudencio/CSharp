734e0f497665b5bbb2eb16cfe8d816260bed0798
brenogaudencio <brenobichels123@gmail.com>
2025-11-25 19:49:53 -0300

    if else

---
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaCondicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+
+            Console.WriteLine("Quantas horas são?");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else {
+                Console.WriteLine("Boa tarde");
+            }
+
+        }
+    }
+}
