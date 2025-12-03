5f501bab52276aeaf596da2036856e9fe4e11e5d
brenogaudencio <brenobichels123@gmail.com>
2025-12-03 05:38:10 -0300

    ExResolvido5

---
+++ ExResolvido5/ExResolvido5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExResolvido5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            String[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("MENOR = " + a);
+            }
+            else if (b < a && b < c)
+            {
+                Console.WriteLine("MENOR = " + b);
+            }
+            else
+            {
+                Console.WriteLine("MENOR = " + c);
+            }
+        }
+    }
+}
