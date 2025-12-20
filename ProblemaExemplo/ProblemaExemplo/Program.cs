00f13c1987023c1ab3a8daa0bf7488f4ac59a724
brenogaudencio <brenobichels123@gmail.com>
2025-12-20 10:07:56 -0300

    ExDoWhile

---
+++ ProblemaExemplo/ProblemaExemplo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius, fahrenheit;
+            char novoPrograma;
+
+            do{
+                Console.Write("Digite a temperatura em Celcius: ");
+                celcius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                fahrenheit = (celcius * 9.0) / 5 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " + fahrenheit.ToString("F1", CultureInfo.InvariantCulture));
+                Console.Write("Deseja repetir (s/n)? ");
+                novoPrograma = char.Parse(Console.ReadLine());
+            } while (novoPrograma == 's');
+            
+        }
+    }
+}
